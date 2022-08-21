b37f9499e786e5cce547eaedc830603e5ddfe3c3
MinateTina <tinatung509@gmail.com>
2022-08-20 17:43:36 -0700

    Initial Commit

---
+++ UIKitFacebook/UIKitFacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitFacebookApp.swift
+//  UIKitFacebook
+//
+//  Created by Minate on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct UIKitFacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

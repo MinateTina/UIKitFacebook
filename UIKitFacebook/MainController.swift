10b1dd17b2a854c748da92760ec8c61e73d8b432
MinateTina <tinatung509@gmail.com>
2022-08-21 13:59:55 -0700

    Setup StoryHeader, StoryPhotoCells, and GradientLayer for K-pop name

--- UIKitFacebook/MainController.swift
+++ UIKitFacebook/MainController.swift
@@ -31,8 +31,98 @@ class PostCell: LBTAListCell<String> {
 }
 
 
+class StoryHeader: UICollectionReusableView {
+    
+    let storyController = StoryController(scrollDirection: .horizontal)
 
-class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storyController.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Jenny Kim", font: .boldSystemFont(ofSize: 14), textColor: .white)
+    
+    
+    
+    override func setupViews() {
+//        backgroundColor = .red
+        imageView.layer.cornerRadius = 10
+        
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        //VStack UIView() on top of nameLabel as Spacer()
+        //Margins same as padding()
+        stack(UIView(), nameLabel).withMargins(.allSides(8))
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    fileprivate func setupGradientLayer() {
+       
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1]
+//        layer.cornerRadius = 10
+//        clipsToBounds = true
+        
+        layer.addSublayer(gradientLayer)
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+}
+
+
+class StoryController: LBTAListController<StoryPhotoCell,String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = ["jenny1", "jenny2", "jenny3","jenny1", "jenny2", "jenny3"]
+    }
+    
+}
+
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -52,11 +142,10 @@ class MainController: LBTAListController<PostCell, String>, UICollectionViewDele
 import SwiftUI
 
 struct MainPreview: PreviewProvider {
+    
     static var previews: some View {
         ContainerView().edgesIgnoringSafeArea(.all)
     }
-    
-    
     
     struct ContainerView: UIViewControllerRepresentable {
         

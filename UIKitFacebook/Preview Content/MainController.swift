7acbb2c4bdb2c909609ee4ab1974169402d22ca7
MinateTina <tinatung509@gmail.com>
2022-08-20 19:32:18 -0700

    Using ListController as collectionView from LBTATools helps a lot for Vstack and Hstack

---
+++ UIKitFacebook/Preview Content/MainController.swift
@@ -0,0 +1,68 @@
+//
+//  MainController.swift
+//  UIKitFacebook
+//
+//  Created by Minate on 8/20/22.
+//
+
+import UIKit
+import LBTATools
+
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(backgroundColor: .red)
+    let nameLabel = UILabel(text: "Name Label")
+    let dateLabel = UILabel(text: "Friday at 11:11AM")
+    let postTextLabel = UILabel(text: "Here is my post text")
+    let imageViewGrid = UIImageView(backgroundColor: .yellow)
+    
+    override func setupViews() {
+        backgroundColor = .white
+        
+        stack(hstack(imageView.withHeight(40).withWidth(40),
+                     stack(nameLabel, dateLabel), spacing: 8)
+                .padLeft(12).padRight(12).padTop(12),
+              postTextLabel, imageViewGrid, spacing: 8)
+    }
+}
+
+
+
+class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
+        
+        self.items = ["Hello", "World","how","it"]
+    
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
+    }
+}
+
+
+
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+    
+}

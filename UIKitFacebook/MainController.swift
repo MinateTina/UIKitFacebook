5013c62792ff9240d23def2f1a3719addf6f0d28
MinateTina <tinatung509@gmail.com>
2022-08-22 22:56:05 -0700

    Make alpha effects on nav bar along with nav bar position while scrolling down/ up

--- UIKitFacebook/MainController.swift
+++ UIKitFacebook/MainController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 import LBTATools
 
 class PostCell: LBTAListCell<String> {
@@ -31,86 +32,6 @@ class PostCell: LBTAListCell<String> {
 }
 
 
-class StoryHeader: UICollectionReusableView {
-    
-    let storyController = StoryController(scrollDirection: .horizontal)
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .yellow
-        
-        stack(storyController.view)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-}
-
-class StoryPhotoCell: LBTAListCell<String> {
-    
-    override var item: String! {
-        didSet {
-            imageView.image = UIImage(named: item)
-        }
-    }
-    
-    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
-    let nameLabel = UILabel(text: "Jenny Kim", font: .boldSystemFont(ofSize: 14), textColor: .white)
-    
-    
-    
-    override func setupViews() {
-//        backgroundColor = .red
-        imageView.layer.cornerRadius = 10
-        
-        stack(imageView)
-        
-        setupGradientLayer()
-        
-        //VStack UIView() on top of nameLabel as Spacer()
-        //Margins same as padding()
-        stack(UIView(), nameLabel).withMargins(.allSides(8))
-    }
-    
-    let gradientLayer = CAGradientLayer()
-    
-    fileprivate func setupGradientLayer() {
-       
-        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
-        gradientLayer.locations = [0.7, 1]
-//        layer.cornerRadius = 10
-//        clipsToBounds = true
-        
-        layer.addSublayer(gradientLayer)
-        
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        gradientLayer.frame = bounds
-    }
-}
-
-
-class StoryController: LBTAListController<StoryPhotoCell,String>, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: 100, height: view.frame.height - 24)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: 0, left: 12, bottom: 0, right: 12)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.items = ["jenny1", "jenny2", "jenny3","jenny1", "jenny2", "jenny3"]
-    }
-    
-}
-
-
 class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
@@ -129,7 +50,48 @@ class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, U
         collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
         
         self.items = ["Hello", "World","how","it"]
+        
+        setupNavBar()
     
+    }
+    
+    let fbLogoImageView = UIImageView(image: UIImage(named: "fb_logo"), contentMode: .scaleAspectFit)
+    
+    let searchButton = UIButton(title: "Search", titleColor: .black)
+    
+    fileprivate func setupNavBar() {
+        
+        let coverWhiteView = UIView(backgroundColor: .white)
+        view.addSubview(coverWhiteView)
+        coverWhiteView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
+        let safeAreaTop = UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0
+        coverWhiteView.constrainHeight(safeAreaTop)
+        
+        let width = view.frame.width - 120 - 16 - 60
+
+        let titleView = UIView(backgroundColor: .clear)
+        titleView.frame = .init(x: 0, y: 0, width: width, height: 50)
+    
+        
+        titleView.hstack(fbLogoImageView.withWidth(120), UIView(backgroundColor: .white).withWidth(width), searchButton.withWidth(60))
+        
+        navigationItem.titleView = titleView
+//        navigationItem.title = "My NAV BAR"
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let safeAreaTop = UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0
+        
+        let magicalSafeAreaTop: CGFloat = safeAreaTop + (navigationController?.navigationBar.frame.height ?? 0)
+//        print(scrollView.contentOffset.y)
+        
+        let offset = scrollView.contentOffset.y + magicalSafeAreaTop
+        
+        let alpha: CGFloat = 1 - (offset/magicalSafeAreaTop)
+        
+        [fbLogoImageView, searchButton].forEach { $0.alpha = alpha}
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -138,9 +100,7 @@ class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, U
 }
 
 
-
-import SwiftUI
-
+//Preview
 struct MainPreview: PreviewProvider {
     
     static var previews: some View {
@@ -150,7 +110,8 @@ struct MainPreview: PreviewProvider {
     struct ContainerView: UIViewControllerRepresentable {
         
         func makeUIViewController(context: Context) -> some UIViewController {
-            return MainController()
+            
+            return UINavigationController(rootViewController: MainController())
         }
         
         func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
